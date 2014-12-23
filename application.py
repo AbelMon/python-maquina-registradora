8f58428f6ee6f415ac401ff506bad7707448cac8
Marvin Abel <marvinabelmon@gmail.com>
2014-12-23 13:49:59 -0600

    En este código se corrigen posibles errores del usuario al ingresar un item

--- application.py
+++ application.py
@@ -1,65 +1,87 @@
 """This is a cash register program."""
-articles={}
+import sys
+articles = {}
+review = []
+products = []
+
 def addItem():
-	key=raw_input("Add item: ")   
-	value=float(raw_input("Add price: "))
-	articles[key]=value
-
-	question=raw_input("Do you want to insert another article y/n or to write the bill estender -Done-:?")
-
-
-	if question =="Done":
-
-		iva =(value*0.12)
-		total = value+iva
-			
-		print "Total to pay:" , total
-		pass
-		cash = input("cash:  ")
-		
-		change = cash - total
-
-		print "   "
-		print "     YOUR BILL"
-                print "   "
-		print ("___Articles:___")
-		for i in articles:
-
-			print i
-		print ("________________")
-		print ("total     %.2f\t")%total
-		print ("iva       %.2f\t")%iva
-		print ("cash      %.2f\t")%cash
-		print ("________________")
-		print ("change"),change
-		print "Thanks for your purchase:::"
-		print "   "
-		print "   "
-		print "   "
-
-	if question=="y":
-
+	"""Esta funcion permite agregar datos a las listas y diccionarios"""
+	try:
+		key = raw_input("Add item: ")
+		keylow = key.lower()
+		sizeinp = len(key)
+		review(key)
+		if sizeinp > 0:
+			value = float(raw_input("Add price: "))
+			print articles
+			print products
+		else:
+			print "Ingresa algun dato"
+			print articles
+			print products
+			addItem()
+		articles[keylow] = value
+		print articles
+		print products
+		again()
+	except ValueError:
+		print "Ingresa correctamente los datos"
+		print articles
+		print products
 		addItem()
 
-
-	else:
+def changevalue():
+	"""Esta funcion pregunta si se quiere cambiar el valor a un articulo"""
+	print "Quieres cambiar valor"
 	
+def again():
+	"""Esta funcion pregunta si se quieren agregar mas articulos"""
+	question=raw_input("Do you want to insert another article y/n ")
+	questionlower = question.lower()
+	if questionlower == "y" or questionlower == "yes" or questionlower == "y " or questionlower == "yes ":
+		addItem()
+	elif questionlower == "n" or questionlower == "no" or questionlower == "n " or questionlower == "no ":
 		menu()
-	print articles
+	else:
+		print "que quieres que haga?"
+		again()
 
-def sellArt(): 
+def correctinput():
+	"""Esta funcion valua si las entradas son correctas"""
+	print "lol"
+
+
+def sellArt():
 	print "olakase"
 
-def menu():       
-	chooseOpt=0
-	while chooseOpt!=3:
-		print "Press 1 if you want to add an item."
-		print "Press 2 if you want to sell an item."
-		print "Press 3 if you want to exit."
-		chooseOpt=int(raw_input("Choose an option: "))
-		if chooseOpt==1:
-			addItem()
-		elif chooseOpt==2:
-			sellArt()
+def review(key):
+	redefin = articles.has_key(key)
+	if redefin == True:
+		abc = raw_input("lola y/n")
+		cde = abc.lower()
+		return cde
 
+
+def menu():
+	chooseOpt = 0
+	print "Press 1 if you want to add an item."
+	print "Press 2 if you want to sell an item."
+	print "Press 3 if you want to exit."
+	while chooseOpt != 3:
+		try:
+			chooseOpt = int(raw_input("Choose an option: "))
+			if chooseOpt == 1:
+				addItem()
+			elif chooseOpt == 2:
+					sellArt()
+			elif chooseOpt <= 0:
+				print "Valor no valido"
+				chooseOpt
+			elif chooseOpt > 3:
+				print "valor no valido"
+				chooseOpt
+			elif chooseOpt == 3:
+				sys.exit(3)
+		except ValueError:
+			print "Ingresa una opcion valida"
 menu()
