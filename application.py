2c7c71b6d0349eefd7ca306df1a2357fd23af35d
Marvin Abel <marvinabelmon@gmail.com>
2014-12-23 16:48:05 -0600

    En esta versión se corrigen datos de entrada y se valuan datos ya existentes en el diccionario

--- application.py
+++ application.py
@@ -1,37 +1,37 @@
 """This is a cash register program."""
 import sys
 articles = {}
-review = []
+reviewnum = []
 products = []
 
-def addItem():
+def EvaluateItem():
 	"""Esta funcion permite agregar datos a las listas y diccionarios"""
 	try:
 		key = raw_input("Add item: ")
+		global keylow
 		keylow = key.lower()
 		sizeinp = len(key)
-		review(key)
+		review(keylow)
 		if sizeinp > 0:
 			value = float(raw_input("Add price: "))
 			print articles
-			print products
+			print "1"
 		else:
 			print "Ingresa algun dato"
 			print articles
-			print products
-			addItem()
+			print "2"
+			EvaluateItem()
 		articles[keylow] = value
 		print articles
-		print products
+		print "3"
 		again()
 	except ValueError:
 		print "Ingresa correctamente los datos"
 		print articles
-		print products
-		addItem()
+		print ""
+		EvaluateItem()
 
-def changevalue():
-	"""Esta funcion pregunta si se quiere cambiar el valor a un articulo"""
+def addItem():
 	print "Quieres cambiar valor"
 	
 def again():
@@ -39,7 +39,7 @@ def again():
 	question=raw_input("Do you want to insert another article y/n ")
 	questionlower = question.lower()
 	if questionlower == "y" or questionlower == "yes" or questionlower == "y " or questionlower == "yes ":
-		addItem()
+		EvaluateItem()
 	elif questionlower == "n" or questionlower == "no" or questionlower == "n " or questionlower == "no ":
 		menu()
 	else:
@@ -57,9 +57,21 @@ def sellArt():
 def review(key):
 	redefin = articles.has_key(key)
 	if redefin == True:
-		abc = raw_input("lola y/n")
+		abc = raw_input("quieres modificar algun dato? ")
 		cde = abc.lower()
-		return cde
+		if cde == "y" or cde == "yes":
+			try:
+				value = float(raw_input("Add new price: "))
+				reviewnum = value
+				articles[keylow] = value
+			except:
+				print "ingresa algun dato"
+		elif cde == "no" or cde == "n":
+			again()
+		else:
+			print "que qieres que haga"
+			review(key)
+
 
 
 def menu():
@@ -71,7 +83,7 @@ def menu():
 		try:
 			chooseOpt = int(raw_input("Choose an option: "))
 			if chooseOpt == 1:
-				addItem()
+				EvaluateItem()
 			elif chooseOpt == 2:
 					sellArt()
 			elif chooseOpt <= 0:
