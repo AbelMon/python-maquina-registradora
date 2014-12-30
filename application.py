b7c16153bb537ef01ca33b467f34b1f8b9581d82
Marvin Abel <marvinabelmon@gmail.com>
2014-12-30 05:02:17 -0600

    Se definió una nueva función para que el usuario pueda ingresar la cantidad de artículos en existencia

--- application.py
+++ application.py
@@ -1,34 +1,58 @@
 """This is a cash register program."""
 import sys
 articles = {}
+InStock = {}
 reviewnum = []
 products = []
 
-def EvaluateItem():
+
+def AddItem():
+	print "Type 'inventory' in 'Add item', to see the added items currently."
 	print "------- Add to Inventory -------"
+	quantity ()
 	arti()
 	price()
+	print articles
+	FunctionInstock()
 	again()
 
-
-def addItem():
-	print "Quieres cambiar valor"
+def ItemMenu():
+	print "instructions"
 	
 def again():
 	"""Esta funcion pregunta si se quieren agregar mas articulos"""	
 	question=raw_input("Do you want to insert another article y/n ")
 	questionlower = question.lower()
 	if questionlower == "y" or questionlower == "yes" or questionlower == "y " or questionlower == "yes ":
-		EvaluateItem()
+		AddItem()
 	elif questionlower == "n" or questionlower == "no" or questionlower == "n " or questionlower == "no ":
 		menu()
 	else:
 		print "que quieres que haga?"
 		again()
 
-def correctinput():
-	"""Esta funcion valua si las entradas son correctas"""
-	print "lol"
+def save():
+	questionsave = raw_input("Save changes? y/n ")
+	savelower = questionsave.lower()
+	if savelower == "y" or savelower == "yes":
+		Inventoryfile = open("inventory.txt","w")
+		Inventoryfile.write("articles")
+		Inventoryfile.close()
+	else:
+		print "Que quieres que haga?"
+		save()
+
+
+def quantity():
+	try:
+		global numbarticles
+		numbarticles = int(raw_input("Cantidad de articles: "))
+		if numbarticles < 0:
+			print "Fuera de rango"
+			quantity()
+	except:
+		print "Ingresa datos validos"
+		quantity()
 
 def  arti():
 	key = raw_input("Add item: ")
@@ -39,15 +63,20 @@ def  arti():
 		print "Ingresa algun dato"
 		arti()
 
+
 def price():
 	try:
 		value = float(raw_input("Add price: "))
 		articles[keylow] = value
 		print articles
+		print InStock
 	except:
 		print "Ingresa correctamente los datos"
 		price()
 
+def FunctionInstock():
+	InStock[keylow] = numbarticles
+	print InStock
 
 
 def review(key):
@@ -55,20 +84,27 @@ def review(key):
 	if sameArt == True:
 		QuestionModif = raw_input("quieres modificar algun dato? ")
 		answer = QuestionModif.lower()
-		if answer == "y" or answer == "yes":
-			try:
-				value = float(raw_input("Add new price: "))
-				articles[keylow] = value
-				again ()
-			except:
-				print "ingresa algun dato"
-		elif answer == "no" or answer == "n":
-			again()
-		else:
-			print "que qieres que haga"
-			review(key)
+		try:
+			if answer == "y" or answer == "yes":
+				newprice()
+			elif answer == "no" or answer == "n":
+				again()
+			else:
+				print "que qieres que haga"
+				review(key)
+		except:
+			print "ingresa algun dato"
+			newprice()
 
-
+def newprice():
+	try:
+		value = float(raw_input("Add new price: "))
+		articles[keylow] = value
+		print articles
+		again()
+	except:
+		print "Ingresa correctamente los datos"
+		newprice()
 
 def menu():
 	chooseOpt = 0
@@ -79,7 +115,7 @@ def menu():
 		try:
 			chooseOpt = int(raw_input("Choose an option: "))
 			if chooseOpt == 1:
-				EvaluateItem()
+				AddItem()
 			elif chooseOpt == 2:
 					sellArt()
 			elif chooseOpt <= 0:
