2e3918ac974775b0e8f16ee5ba8608676b5c3b8a
Marvin Abel <marvinabelmon@gmail.com>
2015-01-14 14:30:28 -0600

    Version testeada, se añaden la opcion para vender artículos, añadir dinero en efectivo, tarjetas de descuento, e imprimir factura.

--- application.py
+++ application.py
@@ -1,37 +1,49 @@
 """This is a cash register program."""
 import sys
 import os
-ARTICLES = {"pera":2.0, "manzana":2.5, "sandia":12, "melon":10.0, "naranja":1.5, "tomate":0.75, "shampoo":19.5, "jabon":7.5}
-InStock = {"pera":28, "manzana":37, "sandia":50, "melon":15, "naranja":80, "tomate":60, "shampoo":17, "jabon":20}
-selllist = []
-goldsil = []
-selldic = {}
-subtotaldic = {}
+ARTICLES = {
+    "pera":2.0, "manzana":2.5,
+    "sandia":12, "melon":10.0,
+    "naranja":1.5, "tomate":0.75, "shampoo":19.5, "jabon":7.5
+}
 
-def Menu():
+INSTOCK = {
+    "pera":28, "manzana":37, "sandia":50,
+    "melon":15, "naranja":80,
+    "tomate":60, "shampoo":17, "jabon":20
+}
+
+SELLLIST = []
+GOLDSIL = []
+SELLDIC = {}
+SUBTOTALDIC = {}
+
+def menu():
     """Main menu"""
-    chooseOpt = 0
+    chooseopt = 0
     print ""
     print "====================Menu===================="
     print "Press 1 if you want to add an item."
     print "Press 2 if you want to sell an item."
     print "Press 3 if you want to exit."
     print ""
-    while chooseOpt != 3:
+    while chooseopt != 3:
         try:
-            chooseOpt = int(raw_input("Choose an option: "))
-            if chooseOpt == 3:
-                sys.exit(1)
-            elif chooseOpt == 1:
+            chooseopt = int(raw_input("Choose an option: "))
+            if chooseopt == 3:
                 clear()
-                AddItem()
-            elif chooseOpt == 2:
+                sys.exit(1)
+            elif chooseopt == 1:
+                clear()
+                additem()
+            elif chooseopt == 2:
                 clear()
                 print "       ---->>>>>>>Selling items<<<<<<<----       "
-                sellArt()
-            elif chooseOpt <= 0:
+                print ""
+                sellart()
+            elif chooseopt <= 0:
                 print "You should enter NUMBERS from ONE to THREE."
-            elif chooseOpt > 3:
+            elif chooseopt > 3:
                 print "You should enter NUMBERS from ONE to THREE."
         except ValueError:
             print "You should enter NUMBERS from ONE to THREE."
@@ -39,24 +51,24 @@ def Menu():
 def arti():
     """This function adds items to inventory"""
     key = raw_input(">Add item: ")
-    global keylow
-    keylow = key.lower()
+    global KEYLOW
+    KEYLOW = key.lower()
     viewinventory()
-    review(keylow)
+    review(KEYLOW)
     if key == "":
         print "Type some data."
         arti()
-    elif keylow == "exit":
+    elif KEYLOW == "exit":
         clear()
-        Menu()
+        menu()
 
 def price():
     """This function adds the items price."""
     try:
         value = float(raw_input(">Add price: "))
-        if keylow != "inv":
-            ARTICLES[keylow] = value
-    except:
+        if KEYLOW != "inv":
+            ARTICLES[KEYLOW] = value
+    except ValueError:
         print "Ingresa correctamente los datos"
         price()
 
@@ -64,25 +76,25 @@ def price():
 def quantity():
     """This function adds the quantity of items."""
     try:
-        global numbarticles
-        numbarticles = int(raw_input(">Amount of articles: "))
-        if keylow != "inv":
-            InStock[keylow] = numbarticles
-        if numbarticles < 0:
+        global NUMBARTICLES
+        NUMBARTICLES = int(raw_input(">Amount of articles: "))
+        if KEYLOW != "inv":
+            INSTOCK[KEYLOW] = NUMBARTICLES
+        if NUMBARTICLES < 0:
             print "Out of Range!"
             quantity()
-    except:
+    except ValueError:
         print "You should enter numbers."
         quantity()
 
 
 def review(key):
     """Check if there are the same items."""
-    sameArt = ARTICLES.has_key(key)
-    if sameArt == True:
-        print "     *The article '%s' already exists. " % (keylow)
-        QuestionModif = raw_input("     *Would you like change the data? y/n ")
-        answer = QuestionModif.lower()
+    sameart = ARTICLES.has_key(key)
+    if sameart == True:
+        print "     *The article '%s' already exists. " % (KEYLOW)
+        questionmodif = raw_input("     *Would you like change the data? y/n ")
+        answer = questionmodif.lower()
         if answer == "y" or answer == "yes":
             quantity()
             price()
@@ -99,50 +111,50 @@ def again():
     question = raw_input("**/Do you want to insert another article? y/n ")
     questionlower = question.lower()
     if questionlower == "y" or questionlower == "yes":
-        AddItem()
+        additem()
     elif questionlower == "n" or questionlower == "no" or questionlower == "not":
         clear()
-        Menu()
+        menu()
     else:
-        print "Enter 'y' to add items or 'n' to return to Menu."
+        print "Enter 'y' to add items or 'n' to return to menu."
         again()
 
 def viewinventory():
     """This function displays the inventory."""
-    if keylow == "inv":
+    if KEYLOW == "inv":
         if len(ARTICLES) != 0:
             clear()
-            Klist = ARTICLES.keys()
+            klist = ARTICLES.keys()
             print "=================INVENTORY================="
-            for item in Klist:
-                print "Article:", item, " Quantity:", InStock[item], " Price:", ARTICLES[item]
+            for item in klist:
+                print "Article:", item, " Quantity:", INSTOCK[item], " Price:", ARTICLES[item]
             print ""
             questinvent()
         else:
             print ""
             print "     /-No items in inventory. Please add items."
-            AddItem()
+            additem()
 
 def questinvent():
     """Submenu of the inventory function"""
     print "/Press 'a' to add more items."
     print "/Press 'd' to delete items."
-    print "/Press 'm' to go to the main Menu."
+    print "/Press 'm' to go to the main menu."
     inventoryquest = raw_input("What you want to do? ")
     questinvlow = inventoryquest.lower()
-    if inventoryquest == "a":
+    if questinvlow == "a":
         clear()
-        AddItem()
-    elif inventoryquest == "d" or inventoryquest == "del":
+        additem()
+    elif questinvlow == "d" or questinvlow == "del":
         clear()
         print "    >>>>>>>>>>>>>>>DELETE<<<<<<<<<<<<<<<    "
         print "Type the name of the item you want to delete."
         print "Type 'inv' to see the items in the inventory."
         print "Type 'cancel' to return to 'Inventory'"
         delitem()
-    elif inventoryquest == "m":
+    elif questinvlow == "m":
         clear()
-        Menu()
+        menu()
     else:
         print "   ***Invalid data***   "
         questinvent()
@@ -155,7 +167,7 @@ def delitem():
     itemexist = ARTICLES.has_key(lowerdelet)
     if itemexist == True:
         del ARTICLES[lowerdelet]
-        del InStock[lowerdelet]
+        del INSTOCK[lowerdelet]
         print "      /*/*/*-ITEM DELETED!"
         noitm()
         print ""
@@ -165,9 +177,9 @@ def delitem():
         delitem()
     elif lowerdelet == "inv":
         noitm()
-        Klist = ARTICLES.keys()
-        for item in Klist:
-            print "  Article:", item, " Quantity:", InStock[item], " Price:", ARTICLES[item]
+        klist = ARTICLES.keys()
+        for item in klist:
+            print "  Article:", item, " Quantity:", INSTOCK[item], " Price:", ARTICLES[item]
         print ""
         print "Type the name of the item you want to delete."
         print "Type 'inv' to see the items in the inventory."
@@ -176,9 +188,8 @@ def delitem():
         delitem()
     elif lowerdelet == "add":
         clear()
-        AddItem()
+        additem()
     elif lowerdelet == "cancel":
-        keylow = "inv"
         viewinventory()
     else:
         print "      /*/*/*-Invalid instruction! The item is not in inventory!"
@@ -191,11 +202,11 @@ def delitem():
 
 def noitm():
     """Displays a message when there are no items in inventory."""
-    artlong = len(InStock)
+    artlong = len(INSTOCK)
     if artlong == 0:
         print "      /*/*/*-No items in inventory."
         print "Please add items."
-        AddItem()
+        additem()
 
 def clear():
     """Cleans the data on screen."""
@@ -204,7 +215,7 @@ def clear():
     elif os.name == ("nt"):
         os.system("cls")
 
-def AddItem():
+def additem():
     """Contains functions 'arti', 'price', 'quantity', 'again'."""
     print ""
     print "====================Add to Inventory===================="
@@ -216,151 +227,235 @@ def AddItem():
     price()
     again()
 
-def sellArt():
+def sellart():
     """Main function to sell items."""
     sale = raw_input(" >>Item to Sell: ")
-    global saleminus
-    saleminus = sale.lower()
-    if saleminus == "done":
-        countingsell(selllist)
+    print ""
+    global SALEMINUS
+    SALEMINUS = sale.lower()
+    if SALEMINUS == "done":
+        countingsell(SELLLIST)
         bill()
         newdict()
         revgld()
         revslv()
         billnormal()
-    elif saleminus == "gold":
-        goldsil.append("gold")
-        print "gold card"
-        sellArt()
-    elif saleminus == "silver":
-        goldsil.append("silver")
-        print "silver card"
-        sellArt()
+    elif SALEMINUS == "gold":
+        GOLDSIL.append("gold")
+        print "*<Gold card has been added.>*"
+        print ""
+        sellart()
+    elif SALEMINUS == "silver":
+        GOLDSIL.append("silver")
+        print "*<Silver card has been added.>*"
+        print ""
+        sellart()
+    elif SALEMINUS == "exit":
+        deletedata()
+        clear()
+        menu()
     else:
-        existindic(saleminus)
+        existindic(SALEMINUS)
 
-def existindic(x):
+def existindic(vararg):
     """Check the existence of items for sale."""
-    itmtrue = ARTICLES.has_key(x)
+    itmtrue = ARTICLES.has_key(vararg)
     if itmtrue == True:
-        selllist.append(x)
+        SELLLIST.append(vararg)
         noexist()
-        sellArt()
+        sellart()
     else:
         print "No existe"
-        sellArt()
+        sellart()
 
-def countingsell(y):
+def countingsell(counting):
     """Creates a dictionary that saves the items sold."""
-    for variable in selllist:
-        selldic[variable] = y.count(variable)
+    for variable in SELLLIST:
+        SELLDIC[variable] = counting.count(variable)
 
 
 def noexist():
     """Subtract 1 when an item is sold"""
-    InStock[saleminus] -= 1
+    INSTOCK[SALEMINUS] -= 1
 
 
 def bill():
+    """Generates a copy of the dictionary "articles" with value zero on items that did not sell."""
     artln = len(ARTICLES)
-    slln = len(selldic)
+    slln = len(SELLDIC)
     if artln >= slln:
         artst = set(ARTICLES)
-        stsell = set(selldic)
+        stsell = set(SELLDIC)
         dif = artst - stsell
-        global lstdif
-        lstdif = list(dif)
-        global artcop
-        artcop = ARTICLES.copy()
-        for var in artcop:
+        global LSTDIF
+        LSTDIF = list(dif)
+        global ARTCOP
+        ARTCOP = ARTICLES.copy()
+        for var in ARTCOP:
             if var > 0:
-                for var in lstdif:
-                    artcop[var] = 0
+                for var in LSTDIF:
+                    ARTCOP[var] = 0
 
 def newdict():
-    global copSell
-    copSell = selldic.copy()
-    stcop = set(artcop)
-    stcopsll = set(copSell)
+    """Generates a copy of the dictionary "SELLDIC" with zero on items that did not sell."""
+    global COPSELL
+    COPSELL = SELLDIC.copy()
+    stcop = set(ARTCOP)
+    stcopsll = set(COPSELL)
     dif = stcop - stcopsll
     lsta = list(dif)
     for var in lsta:
-        copSell[var] = 0
+        COPSELL[var] = 0
 
 def revgld():
-    if "gold" in goldsil:
+    """Generates total to pay for the gold card."""
+    if "gold" in GOLDSIL:
         total = 0
         grantotal = 0
-        for var in artcop:
-            total = total + artcop[var] * copSell[var]
+        for var in ARTCOP:
+            total = total + ARTCOP[var] * COPSELL[var]
         total = round(total / 1.05, 2)
         grantotal = round(total *1.12, 2)
         subtotalgld()
+        print " ----->>Total: ", grantotal
+        print ""
+        print "      ///---Payment"
+        print ""
+        money(grantotal)
 
 
 def revslv():
-    if "silver" in goldsil:
+    """Generates total to pay for the silver card."""
+    if "silver" in GOLDSIL:
         total = 0
         grantotal = 0
-        for var in artcop:
-            total = total + artcop[var] * copSell[var]
+        for var in ARTCOP:
+            total = total + ARTCOP[var] * COPSELL[var]
         total = round(total / 1.02, 2)
         grantotal = round(total * 1.12, 2)
         subtotalslv()
-        print total
-        print grantotal
+        print " ----->>Total: ", grantotal
+        print ""
+        print "      ///---Payment"
+        print ""
+        money(grantotal)
 
 
 def billnormal():
+    """Generates total to pay in a normal sale."""
     total = 0
     grantotal = 0
-    for var in artcop:
-        total = total + artcop[var] * copSell[var]
+    for var in ARTCOP:
+        total = total + ARTCOP[var] * COPSELL[var]
     grantotal = round(total * 1.12, 2)
     print ""
     print " ----->>Total: ", grantotal
     subtotal()
     print ""
-    print "    ///---Payment"
+    print "      ///---Payment"
+    print ""
     money(grantotal)
-    clear()
-
 
 def subtotal():
-    for var in copSell:
-        subtotaldic[var] = round((artcop[var] * copSell[var]) * 1.12, 2)
+    """Stores the subtotal of each item sold."""
+    for var in COPSELL:
+        SUBTOTALDIC[var] = round((ARTCOP[var] * COPSELL[var]) * 1.12, 2)
 
 
 def subtotalslv():
-    for var in copSell:
-        subtotaldic[var] = round(((artcop[var] * copSell[var]) / 1.02) * 1.12, 2)
+    """Stores the subtotal of each item sold with silver card."""
+    for var in COPSELL:
+        SUBTOTALDIC[var] = round(((ARTCOP[var] * COPSELL[var]) / 1.02) * 1.12, 2)
 
 def subtotalgld():
-    for var in copSell:
-        subtotaldic[var] = round(((artcop[var] * copSell[var]) / 1.05) * 1.12, 2)
-    print subtotaldic
-    print "yolo"
+    """Stores the subtotal of each item sold with gold card."""
+    for var in COPSELL:
+        SUBTOTALDIC[var] = round(((ARTCOP[var] * COPSELL[var]) / 1.05) * 1.12, 2)
+
+def newsell():
+    """This function asks the user to make a new sale."""
+    print "  //-Type 'new' to sell again."
+    print "  //-Type 'exit' to return to the main menu."
+    print ""
+    what = raw_input("       > What you want to do? ")
+    whatmin = what.lower()
+    if whatmin == "new":
+        deletedata()
+        clear()
+        print "       ---->>>>>>>Selling items<<<<<<<----       "
+        print ""
+        sellart()
+    elif whatmin == "exit":
+        deletedata()
+        clear()
+        menu()
+    else:
+        print "           > Invalid data!"
+        newsell()
+
+def numtrue(revkey):
+    """This function checks if the argument can be converted to float."""
+    try:
+        float(revkey)
+        return True
+    except ValueError:
+        return False
 
 
 def money(gran):
-    mon = float(raw_input(" >>>Cash: "))
-    if mon > 0:
-        if mon >= gran:
-            invoice()
-        elif mon < gran:
+    """This function stores the cash to generate the invoice."""
+    mon = raw_input("       >Cash: ")
+    bol = mon.isalpha()
+    condic = numtrue(mon)
+    print bol
+    if bol == False and mon != "" and condic == True:
+        monnum = float(mon)
+        if monnum >= 0:
+            if monnum >= gran:
+                invoice(monnum, gran)
+            elif monnum < gran:
+                print ""
+                print "Not enough money"
+                money(gran)
+        elif monnum < 0:
             print ""
-            print "Not enough money"
+            print "Invalid data."
             money(gran)
+        else:
+            print "Invalid data."
+            money(gran)
+    else:
+        print "pera"
+        money(gran)
 
-def solvent():
-	print "s"
 
-def invoice():
-    print "____________________INVOICE____________________"
+def deletedata():
+    """Clears the data stored in lists and dictionaries, for use on a new sale."""
+    del SELLLIST[0:]
+    del GOLDSIL[0:]
+    SELLDIC.clear()
+
+
+def invoice(efect, total):
+    """Invoice with totals, subtotals, cash and change."""
+    clear()
+    print "  ____________________INVOICE____________________"
     print ""
-    print "----------Items----------"
+    for var in COPSELL:
+        if COPSELL[var] > 0:
+            if SUBTOTALDIC[var] > 0:
+                print "    -", str(COPSELL[var]), str(var), ".............."\
+                "subtotal: ", str(SUBTOTALDIC[var])
+    print ""
+    print "       ---------------------------------"
+    print "                          ", "Total: ", str(total)
+    print "                          ", "Cash:  ", str(efect)
+    print "                          ", "Change:", str(efect - total)
+    print "     --> Thank you for shopping with us!"
+    print ""
+    print "       ---------------------------------"
+    print ""
+    newsell()
 
-
-
-
-Menu()
+clear()
+menu()
