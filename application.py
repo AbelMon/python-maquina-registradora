cd0aacfe89cc81dd023dd849c1c3b65c9d7047f9
Marvin Abel <marvinabelmon@gmail.com>
2015-01-21 17:06:20 -0600

    Change Code

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
 """This is a cash register program."""
 import sys
-<<<<<<< HEAD
 import os
 ARTICLES = {
     "pera":2.0, "manzana":2.5,
@@ -62,49 +61,41 @@ def arti():
     elif KEYLOW == "exit":
         clear()
         menu()
-=======
-articles = {}
-InStock = {}
-reviewnum = []
-products = []
+
+def additem():
+    print ""
+    print "            ------- Add to Inventory -------"
+    print ""
+    print "Type 'inv' in 'Add item', to see the added items currently."
+    arti()
+    quantity ()
+    price()
+    again()
 
 
-def AddItem():
-	print "Type 'inventory' in 'Add item', to see the added items currently."
-	print "------- Add to Inventory -------"
-	quantity ()
-	arti()
-	price()
-	print articles
-	FunctionInstock()
-	again()
-
-def ItemMenu():
-	print "instructions"
-	
 def again():
-	"""Esta funcion pregunta si se quieren agregar mas articulos"""	
-	question=raw_input("Do you want to insert another article y/n ")
-	questionlower = question.lower()
-	if questionlower == "y" or questionlower == "yes" or questionlower == "y " or questionlower == "yes ":
-		AddItem()
-	elif questionlower == "n" or questionlower == "no" or questionlower == "n " or questionlower == "no ":
-		menu()
-	else:
-		print "que quieres que haga?"
-		again()
+    """Esta funcion pregunta si se quieren agregar mas articulos""" 
+    question=raw_input("Do you want to insert another article y/n ")
+    questionlower = question.lower()
+    if questionlower == "y" or questionlower == "yes" or questionlower == "y " or questionlower == "yes ":
+        additem()
+    elif questionlower == "n" or questionlower == "no" or questionlower == "n " or questionlower == "no ":
+        menu()
+    else:
+        print "que quieres que haga?"
+        again()
 
 def save():
-	questionsave = raw_input("Save changes? y/n ")
-	savelower = questionsave.lower()
-	if savelower == "y" or savelower == "yes":
-		Inventoryfile = open("inventory.txt","w")
-		Inventoryfile.write("articles")
-		Inventoryfile.close()
-	else:
-		print "Que quieres que haga?"
-		save()
->>>>>>> b7c16153bb537ef01ca33b467f34b1f8b9581d82
+    questionsave = raw_input("Save changes? y/n ")
+    savelower = questionsave.lower()
+    if savelower == "y" or savelower == "yes":
+        Inventoryfile = open("inventory.txt","w")
+        Inventoryfile.write("articles")
+        Inventoryfile.close()
+    else:
+        print "Que quieres que haga?"
+        save()
+
 
 def price():
     """This function adds the items price."""
@@ -131,7 +122,6 @@ def quantity():
         print "You should enter numbers."
         quantity()
 
-<<<<<<< HEAD
 
 def review(key):
     """Check if there are the same items."""
@@ -150,27 +140,7 @@ def review(key):
         else:
             print "Enter 'y' to modify data or 'n' to discard changes."
             review(key)
-=======
-def quantity():
-	try:
-		global numbarticles
-		numbarticles = int(raw_input("Cantidad de articles: "))
-		if numbarticles < 0:
-			print "Fuera de rango"
-			quantity()
-	except:
-		print "Ingresa datos validos"
-		quantity()
 
-def  arti():
-	key = raw_input("Add item: ")
-	global keylow
-	keylow = key.lower()
-	review(keylow)
-	if key == "":
-		print "Ingresa algun dato"
-		arti()
->>>>>>> b7c16153bb537ef01ca33b467f34b1f8b9581d82
 
 def again():
     """Esta funcion pregunta si se quieren agregar mas articulos"""
@@ -185,7 +155,7 @@ def again():
         print "Enter 'y' to add items or 'n' to return to menu."
         again()
 
-<<<<<<< HEAD
+
 def viewinventory():
     """This function displays the inventory."""
     if KEYLOW == "inv":
@@ -282,18 +252,6 @@ def clear():
     elif os.name == ("nt"):
         os.system("cls")
 
-def additem():
-    """Contains functions 'arti', 'price', 'quantity', 'again'."""
-    print ""
-    print "====================Add to Inventory===================="
-    print "Type 'inv' in 'Add item', to see the added items currently."
-    print "Type 'exit' in 'Add item' to go to main menu."
-    print ""
-    arti()
-    quantity()
-    price()
-    again()
-
 def sellart():
     """Main function to sell items."""
     sale = raw_input(" >>Item to Sell: ")
@@ -332,7 +290,7 @@ def existindic(vararg):
         noexist()
         sellart()
     else:
-        print "No existe"
+        print "      >*Invalid data! The item is not in inventory!"
         sellart()
 
 def countingsell(counting):
@@ -474,7 +432,6 @@ def money(gran):
     mon = raw_input("       >Cash: ")
     bol = mon.isalpha()
     condic = numtrue(mon)
-    print bol
     if bol == False and mon != "" and condic == True:
         monnum = float(mon)
         if monnum >= 0:
@@ -492,7 +449,7 @@ def money(gran):
             print "Invalid data."
             money(gran)
     else:
-        print "pera"
+        print "Invalid data!"
         money(gran)
 
 
@@ -526,70 +483,3 @@ def invoice(efect, total):
 
 clear()
 menu()
-=======
-def price():
-	try:
-		value = float(raw_input("Add price: "))
-		articles[keylow] = value
-		print articles
-		print InStock
-	except:
-		print "Ingresa correctamente los datos"
-		price()
-
-def FunctionInstock():
-	InStock[keylow] = numbarticles
-	print InStock
-
-
-def review(key):
-	sameArt = articles.has_key(key)
-	if sameArt == True:
-		QuestionModif = raw_input("quieres modificar algun dato? ")
-		answer = QuestionModif.lower()
-		try:
-			if answer == "y" or answer == "yes":
-				newprice()
-			elif answer == "no" or answer == "n":
-				again()
-			else:
-				print "que qieres que haga"
-				review(key)
-		except:
-			print "ingresa algun dato"
-			newprice()
-
-def newprice():
-	try:
-		value = float(raw_input("Add new price: "))
-		articles[keylow] = value
-		print articles
-		again()
-	except:
-		print "Ingresa correctamente los datos"
-		newprice()
-
-def menu():
-	chooseOpt = 0
-	print "Press 1 if you want to add an item."
-	print "Press 2 if you want to sell an item."
-	print "Press 3 if you want to exit."
-	while chooseOpt != 3:
-		try:
-			chooseOpt = int(raw_input("Choose an option: "))
-			if chooseOpt == 1:
-				AddItem()
-			elif chooseOpt == 2:
-					sellArt()
-			elif chooseOpt <= 0:
-				print "Valor no valido"
-				chooseOpt
-			elif chooseOpt > 3:
-				print "valor no valido"
-				chooseOpt
-			elif chooseOpt == 3:
-				sys.exit(0)
-		except ValueError:
-			print "Ingresa una opcion valida"
-menu()
->>>>>>> b7c16153bb537ef01ca33b467f34b1f8b9581d82
